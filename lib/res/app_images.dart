1b64be3c9e3ca733c6ec120d595eb7e82b2a76c2
Mahmoud_Madih <moodbeder150@gmail.com>
2024-07-20 12:34:47 +0300

    finsh app to load from loacl strorage

---
+++ lib/res/app_images.dart
@@ -0,0 +1,17 @@
+class AppImages {
+  static const imageList={
+    0: 'assets/images/1s.jpg',
+    1: 'assets/images/2s.jpg',
+    2: 'assets/images/3s.jfif',
+    3: 'assets/images/4s.jpg',
+    4: 'assets/images/5s.jfif',
+    5: 'assets/images/6s.jfif',
+    6: 'assets/images/8s.jfif',
+    7: 'assets/images/9s.jfif',
+    8: 'assets/images/10s.jfif',
+    9: 'assets/images/67s.jfif',
+    10: 'assets/images/11.png',
+    11: 'assets/images/songer.jpg',
+    12: 'assets/images/13.png',
+  };
+}
